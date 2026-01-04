46b6db07e58ea3c326705e0a9b74ac564fe226c2
vzbel <142253332+vzbel@users.noreply.github.com>
2026-01-04 16:45:22 -0700

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import Header from './components/Header';
 
 function App() {
-
   return (
-    <>
-    Welcome
-    </>
-  )
-}
+    <div className="wrapper">
+      <Header
+        imageUrl="https://www.gamerevolution.com/wp-content/uploads/sites/2/2023/09/cs2-ranks-ranking-system-vs-csgo.jpg"
+        imageAlt="FPS video game showing AK-47 being shot down an alley"
+        title="CSGO Base Weapons"
+        description="Explore all of CSGO’s base weaponry,
+        including favorites like the 
+        AK-47, M4A1-S, and more!"
+        ctaText="API Website"
+        ctaLink="https://bymykel.com/CSGO-API/"
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import "../styles/Header.css";
+
+// Hero section including a call-to-action (CTA) button
+const Header = (props) => {
+    return (
+        <header className="flex">
+            <div className="header-left">
+                <img 
+                    src={props.imageUrl} 
+                    alt={props.imageAlt}
+                    className="header-img" 
+                />
+            </div>
+            <div className="header-right">
+                <h1>{props.title}</h1>
+                <p className="text-faded">{props.description}</p>
+                <a href={props.ctaLink} >
+                    <button className="header-btn">{props.ctaText}</button>
+                </a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
